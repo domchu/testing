28e23ef51723112fab8b0fe825d3aa738ba563ea
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-08-20 21:19:50 +0100

    chore:created the form interface

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import UserForm from './UserForm';
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <UserForm/>
+    </>
+  )
+}
+
+export default App
---
+++ src/UserForm.tsx
@@ -0,0 +1,17 @@
+
+const UserForm = () => {
+  return (
+      <form >
+          <div>
+              <label>Name</label>
+        <input />
+      <br />
+              <label>Name</label>
+              <input  />
+          
+          </div> 
+    </form>
+  )
+}
+
+export default UserForm
