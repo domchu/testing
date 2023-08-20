2d8636430eccc2b4361445b7c8a67cc74a532185
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-08-20 21:44:18 +0100

    chore:output the data to the console

--- src/UserForm.tsx
+++ src/UserForm.tsx
@@ -1,18 +1,30 @@
+import { useState } from "react";
 
 const UserForm = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  // let event : string
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+
+    console.log(name, email);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
         <label>Name</label>
-        <input />
+        <input value={name} onChange={(e) => setName(e.target.value)} />
       </div>
       <div>
-        <label>Name</label>
-        <input />
+        <label>Email</label>
+        <input value={email} onChange={(e) => setEmail(e.target.value)} />
       </div>
       <button>Add User</button>
     </form>
   );
-}
+};
 
-export default UserForm
+export default UserForm;
